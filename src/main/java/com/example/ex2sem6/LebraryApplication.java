4e0b89f1ed5a3651cf3e3cbc700db5960284875c
Vladimir <120101620+RybalchenkoV4@users.noreply.github.com>
2025-02-14 17:11:17 +0300

    Update LebraryApplication.java

--- src/main/java/com/example/ex2sem6/LebraryApplication.java
+++ src/main/java/com/example/ex2sem6/LebraryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ex2sem6Application.class, args);
+		SpringApplication.run(LibraryApplication.class, args);
 	}
 
 }
