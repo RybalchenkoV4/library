404018be84d5d5a475cacd0aff68816c208d66bf
Vladimir <120101620+RybalchenkoV4@users.noreply.github.com>
2025-02-14 17:11:03 +0300

    Update and rename Ex2sem6Application.java to LebraryApplication.java
    commit

--- src/main/java/com/example/ex2sem6/LebraryApplication.java
+++ src/main/java/com/example/ex2sem6/LebraryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex2sem6Application {
+public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ex2sem6Application.class, args);
